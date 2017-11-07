4ab6a26ebd78d40bda2b64fa07bdf203df3bdb36
eldiablotrabajo <alexbiddle72@gmail.com>
2017-11-07 20:10:58 +0000

    Added basic introduction and do loop for asking user if they want to continue with game

--- MyTextGame/Program.cs
+++ MyTextGame/Program.cs
@@ -10,6 +10,47 @@ namespace MyTextGame
     {
         static void Main(string[] args)
         {
+            //strings
+            string playerName;
+            string playerChoice;
+            int correct = 0;
+
+
+            Console.WriteLine("Hello good sir/Madam please enter your name:");
+            playerName = Console.ReadLine();
+            Console.WriteLine("Ahh yes " + playerName + " the name of a warrior, I have a challenge for you" +
+                " if you can slay the 3 beasts located in this without dying a horrible death I will grant you one wish");
+            
+
+            do
+            {
+                Console.WriteLine("Are you up for the challenge?:");
+                playerChoice = Console.ReadLine().ToUpper();
+                if (playerChoice == "YES")
+                {
+                    Console.WriteLine("Fantastic let us continue on with our adventure! First we need to pick a weapon for you!");
+                    correct = 1;
+                    
+                }
+                else if (playerChoice == "NO")
+                {
+                    Console.WriteLine("Ahhh ok greater men than you have failed " + playerName);
+                    correct = 1;
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Error please try again");
+                    correct = 0;
+                }
+
+            } while (correct == 0);
+           
+
+
+                
+
+
         }
     }
 }
